db9a05e3d3737ab77bd54ac3390d49d696d8b941
unknown <shamon T>
2024-08-05 11:16:12 +0530

    first committ

--- server/socketroutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { user_list, signup, signin } = require("../controllers/user.controller");
-const router = express.Router();
-const jwt = require("jsonwebtoken");
-
-// router.get("/", user_list);
-router.post("/sign-up", signup);
-
-module.exports = router;
